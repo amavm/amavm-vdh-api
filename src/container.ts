d6c7559cf5fca8d5267556c4487a502bb2482dc5
Pierre Buyle <pbuyle@hopper.com>
2020-03-02 22:12:31 -0500

    Add basic auth service to get authorization token ad authenticated user (if any)

--- src/container.ts
+++ src/container.ts
@@ -1,16 +1,32 @@
 import { Config } from "@config";
-import { AssetsService, DummyLocalAssetsService, S3AssetsService } from "@services/assets.service";
-import { BicyclePathsService, MongoDbBicyclePathsService } from "@services/bicycle-paths.service";
-import { GeoSourceService, MTLOpenDataGeoSourceService } from "@services/geo-source.service";
-import { MongoDbObservationsService, ObservationsService } from "@services/observations.service";
+import {
+  AssetsService,
+  DummyLocalAssetsService,
+  S3AssetsService
+} from "@services/assets.service";
+import { AuthService, FirebaseAuthService } from "@services/auth.service";
+import {
+  BicyclePathsService,
+  MongoDbBicyclePathsService
+} from "@services/bicycle-paths.service";
+import {
+  GeoSourceService,
+  MTLOpenDataGeoSourceService
+} from "@services/geo-source.service";
+import {
+  MongoDbObservationsService,
+  ObservationsService
+} from "@services/observations.service";
 import { DefaultSyncService, SyncService } from "@services/sync.service";
 import * as uno from "uno-serverless";
-import { httpClientFactory } from "uno-serverless";
+import { httpClientFactory, memoize } from "uno-serverless";
 import * as unoAws from "uno-serverless-aws";
+
+import * as firebaseAdmin from "firebase-admin";
 
 export enum ExecutionMode {
   LocalDev = "localdev",
-  RestOfTheWorld = "restoftheworld",
+  RestOfTheWorld = "restoftheworld"
 }
 
 /** The specification for the container. */
@@ -23,6 +39,8 @@ export interface Container {
   geoSourceService(): GeoSourceService;
   observationsService(): ObservationsService;
   syncService(): SyncService;
+  firebaseApp(): firebaseAdmin.app.App;
+  authService(): AuthService;
 }
 
 /** Options for container creation. */
@@ -32,42 +50,65 @@ export interface ContainerOptions {
 }
 
 /** Definition of factories for the container. */
-export const createContainer = uno.createContainerFactory<Container, ContainerOptions>({
-  assetsService: ({ container, options }) => options.mode === ExecutionMode.LocalDev
-    ? new DummyLocalAssetsService()
-    : new S3AssetsService({
-      bucket: container.configService().get(Config.AssetsBucket),
-      websiteRoot: container.configService().get(Config.AssetsRoot),
+export const createContainer = uno.createContainerFactory<
+  Container,
+  ContainerOptions
+>({
+  assetsService: ({ container, options }) =>
+    options.mode === ExecutionMode.LocalDev
+      ? new DummyLocalAssetsService()
+      : new S3AssetsService({
+          bucket: container.configService().get(Config.AssetsBucket),
+          websiteRoot: container.configService().get(Config.AssetsRoot)
+        }),
+
+  authService: ({ container }) => new FirebaseAuthService(container.firebaseApp()),
+
+  bicyclePathsService: ({ container }) =>
+    new MongoDbBicyclePathsService({
+      db: container.configService().get(Config.MongoDbDb),
+      url: container.configService().get(Config.MongoDbUrl)
     }),
 
-  bicyclePathsService: ({ container }) => new MongoDbBicyclePathsService({
-    db: container.configService().get(Config.MongoDbDb),
-    url: container.configService().get(Config.MongoDbUrl),
-  }),
-
-  configService: ({ options }) => options.mode === ExecutionMode.LocalDev
-    ? new uno.JSONFileConfigService({ debug: true, path: "./local.config.json" })
-    : new unoAws.SSMParameterStoreConfigService({
-      path: `/amavm/vdh-api/${options.stage}`,
-    }),
+  configService: ({ options }) =>
+    options.mode === ExecutionMode.LocalDev
+      ? new uno.JSONFileConfigService({
+          debug: true,
+          path: "./local.config.json"
+        })
+      : new unoAws.SSMParameterStoreConfigService({
+          path: `/amavm/vdh-api/${options.stage}`
+        }),
 
   environmentName: ({ options }) => options.stage,
 
   executionMode: ({ options }) => options.mode,
 
-  geoSourceService: ({ container }) => new MTLOpenDataGeoSourceService(
-    { bicyclePathsSourceUrl: container.configService().get(Config.MTLOpenDataBicyclePathUrl) },
-    httpClientFactory()),
+  geoSourceService: ({ container }) =>
+    new MTLOpenDataGeoSourceService(
+      {
+        bicyclePathsSourceUrl: container
+          .configService()
+          .get(Config.MTLOpenDataBicyclePathUrl)
+      },
+      httpClientFactory()
+    ),
 
-  observationsService: ({ container }) => new MongoDbObservationsService(
-    {
-      db: container.configService().get(Config.MongoDbDb),
-      url: container.configService().get(Config.MongoDbUrl),
-    },
-    container.assetsService()),
+  observationsService: ({ container }) =>
+    new MongoDbObservationsService(
+      {
+        db: container.configService().get(Config.MongoDbDb),
+        url: container.configService().get(Config.MongoDbUrl)
+      },
+      container.assetsService()
+    ),
 
-  syncService: ({ container }) => new DefaultSyncService(
-    container.assetsService(),
-    container.geoSourceService(),
-    container.bicyclePathsService()),
+  syncService: ({ container }) =>
+    new DefaultSyncService(
+      container.assetsService(),
+      container.geoSourceService(),
+      container.bicyclePathsService()
+    ),
+
+  firebaseApp: () => firebaseAdmin.initializeApp(),
 });
